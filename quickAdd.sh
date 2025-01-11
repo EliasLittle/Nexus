be82ac072b8bf6d9266053230b2206c7d796aac9
Elias Little <eliasjlittle@gmail.com>
2025-01-11 16:43:10 -0500

    Added testing files and script to quickly add data to server on initialization

---
+++ quickAdd.sh
@@ -0,0 +1,15 @@
+./nexus-client publish value /testing/a 123
+./nexus-client publish value /testing/b 456
+./nexus-client publish value /testing/c 789
+./nexus-client publish value /testing/d 101
+./nexus-client publish value /testing/e 102
+./nexus-client publish value /testing/f 103
+./nexus-client publish value /testing/g 104
+./nexus-client publish value /testing/h 105
+./nexus-client publish value /testing/i 106
+./nexus-client publish value /testing/j 107
+
+./nexus-client publish dataset /testing/dataset/a ./tests/example_a.csv
+./nexus-client publish dataset /testing/dataset/b ./tests/example_b.csv
+./nexus-client publish dataset /testing/dataset/c ./tests/example_c.csv
+./nexus-client publish dataset /testing/dataset/d ./tests/example_d.csv
