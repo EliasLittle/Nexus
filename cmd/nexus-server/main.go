965ac2257dd58a435da0546f16e18d91963e6e0c
Elias Little <eliasjlittle@gmail.com>
2025-01-16 16:07:20 -0500

    New feature: Save and load index from disk.

--- cmd/nexus-server/main.go
+++ cmd/nexus-server/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	pb "nexus/pkg/proto"
+	"os"
+	"os/signal"
 
 	ns "nexus/pkg/server"
 
@@ -12,10 +14,57 @@ import (
 )
 
 func main() {
+	log.Println("Starting Nexus Server...")
+
+	var loadFilePath, saveFilePath string
+	if len(os.Args) == 2 {
+		loadFilePath = os.Args[1]
+		saveFilePath = os.Args[1]
+	} else if len(os.Args) > 2 {
+		loadFilePath = os.Args[1]
+		saveFilePath = os.Args[2]
+	} else {
+		log.Printf("Num args: %d", len(os.Args))
+		log.Fatalf("Usage: '%s <load_file_path> <save_file_path>' or '%s <save_file_path>'", os.Args[0], os.Args[0])
+	}
+
+	// Generate full paths for load and save file
+	expandedPath, err := os.UserHomeDir() // Get the user's home directory
+	if err != nil {
+		log.Fatalf("Failed to get home directory: %v", err)
+	}
+	fullLoadPath := fmt.Sprintf("%s/%s", expandedPath, loadFilePath) // Expand the load path
+	fullSavePath := fmt.Sprintf("%s/%s", expandedPath, saveFilePath) // Expand the save path
 
 	// Initialize a new Trie for storing paths
-	var index = ns.NewTrie()
-	log.Println("Starting Nexus Server...")
+	var index *ns.Trie
+	if loadFilePath != "" {
+		log.Printf("Loading index from %s", fullLoadPath)
+		index, err = ns.NewTrie(fullLoadPath) // Load from the provided file path
+	} else {
+		index, err = ns.NewTrie() // No load path provided
+	}
+
+	if err != nil {
+		log.Fatalf("Failed to create new Trie: %v", err)
+	}
+
+	// Create a channel to listen for interrupt signals
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	// Defer saving the trie to the save file on shutdown
+	go func() {
+		<-signalChan            // Wait for interrupt signal
+		if saveFilePath != "" { // Only save if a save path was provided
+			if err := index.SaveToDisk(fullSavePath); err != nil {
+				log.Printf("Failed to save index: %v", err)
+			} else {
+				log.Printf("Saved index to %s", fullSavePath)
+			}
+		}
+		os.Exit(0) // Exit gracefully
+	}()
 
 	// Create listener
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", ns.DefaultPort))
--- pkg/server/dataIndex.go
+++ pkg/server/dataIndex.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	pb "nexus/pkg/proto"
+	"os"
+	"reflect"
 	"strings"
 )
 
@@ -11,15 +14,28 @@ type TrieNode struct {
 	Children    map[string]*TrieNode
 	IsEndOfPath bool
 	Value       interface{} // Store different types of values
+	ValueType   string      // Type identifier for the value
 }
 
 type Trie struct {
 	Root *TrieNode
 }
 
-// NewTrie initializes a new Trie
-func NewTrie() *Trie {
-	return &Trie{Root: &TrieNode{Children: make(map[string]*TrieNode)}}
+// NewTrie initializes a new Trie, optionally loading from a file
+func NewTrie(filepath ...string) (*Trie, error) {
+	if len(filepath) > 0 {
+		log.Printf("Loading from disk: %s", filepath[0])
+		trie, err := LoadFromDisk(filepath[0]) // Load from the provided file path
+		if err != nil {
+			log.Printf("Error loading from disk: %v", err)
+			return nil, err
+		}
+		trie.Traverse()
+		return trie, nil
+	} else {
+		trie := &Trie{Root: &TrieNode{Children: make(map[string]*TrieNode)}}
+		return trie, nil
+	}
 }
 
 // Insert adds a new path to the Trie with an associated value
@@ -35,6 +51,8 @@ func (t *Trie) Insert(path string, value interface{}) {
 	// TODO: Think about if we want to store data in intermediate nodes and not just at the end of the path
 	node.IsEndOfPath = true
 	node.Value = value // Store the value at the end of the path
+	node.ValueType = reflect.TypeOf(value).Elem().Name()
+
 }
 
 // Search checks if a path exists in the Trie
@@ -86,8 +104,12 @@ func (t *Trie) GetType(path string) (string, error) {
 			return "EventStream", nil
 		case *pb.Dataset:
 			return "Dataset", nil
-		case *pb.DirectValue:
-			return "Value", nil
+		case *pb.StringValue:
+			return "StringValue", nil
+		case *pb.IntValue:
+			return "IntValue", nil
+		case *pb.FloatValue:
+			return "FloatValue", nil
 		default:
 			return "Unknown", nil
 		}
@@ -130,6 +152,8 @@ func (t *Trie) GetChildren(path string) []string {
 
 // Get retrieves the value stored at the given path in the Trie
 func (t *Trie) Get(path string) (interface{}, error) {
+	t.Traverse()
+	log.Printf("Get| Path: %s", path)
 	node := t.Root
 	segments := splitPath(path) // Customizable segmenter
 	for _, segment := range segments {
@@ -138,26 +162,133 @@ func (t *Trie) Get(path string) (interface{}, error) {
 		}
 		node = node.Children[segment]
 	}
+	log.Printf("Get| Node: %v", node)
 	if node.IsEndOfPath {
+		log.Printf("Get| Is end of path")
 		return node.Value, nil // Return the value if it exists
 	}
+	log.Printf("Get| Is not end of path")
 	return nil, fmt.Errorf("path exists but has no value: %s", path) // Return an error if the path exists but has no value
 }
 
-/*
-func dataIndexExample() {
-	// Connect to the Nexus server
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+// SaveToDisk saves the current Trie to a file
+func (t *Trie) SaveToDisk(filename string) error {
+	file, err := os.Create(filename)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		return err
 	}
-	defer conn.Close()
+	defer file.Close()
 
-	// Example usage of GetType
-	dataType, err := pathTrie.GetType("/events/sensors")
-	if err != nil {
-		log.Fatalf("Error getting type: %v", err)
-	}
-	log.Printf("Data type at path '/events/sensors': %s", dataType)
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(t)
 }
-*/
+
+// LoadFromDisk initializes a Trie from a file
+func LoadFromDisk(filename string) (*Trie, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var trie Trie
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&trie); err != nil {
+		log.Printf("Error decoding Trie: %v", err)
+		return nil, err
+	}
+
+	log.Printf("Traversing before loading values")
+	trie.Traverse()
+
+	// Load values based on their type identifiers
+	log.Printf("Loading values")
+	loadValues(&trie)
+
+	log.Printf("Traversing after loading values")
+	trie.Traverse()
+
+	return &trie, nil
+}
+
+// loadValues reconstructs the values based on their type identifiers
+func loadValues(trie *Trie) {
+	loadNodeValues(trie.Root)
+	log.Printf("Internal traversal")
+	trie.Traverse()
+}
+
+func loadNodeValues(node *TrieNode) {
+	if node.IsEndOfPath {
+		log.Printf("Loading values for node: %s", node.ValueType)
+		switch node.ValueType {
+		case "EventStream":
+			node.Value = &pb.EventStream{}
+		case "Dataset":
+			// Extract the actual dataset value from the representation
+			log.Printf("Found dataset")
+			switch node.Value.(type) {
+			case string:
+				node.Value = parseDatasetValue(node.Value.(string))
+			case map[string]interface{}:
+				log.Printf("Found dataset as map")
+				//node.Value = parseDatasetValue(node.Value.(map[string]interface{}))
+			default:
+				log.Printf("Failed to extract dataset value: %v", node.Value)
+			}
+		case "StringValue":
+			log.Printf("Found string value")
+			// Extract the actual string value from the representation
+			if valueMap, ok := node.Value.(map[string]interface{}); ok {
+				log.Printf("Successfully found string value as map")
+				node.Value = &pb.StringValue{Value: valueMap["value"].(string)}
+			} else {
+				log.Printf("Failed to extract string value: %v", node.Value)
+			}
+		case "IntValue":
+			log.Printf("Found int value")
+			// Extract the actual integer value from the representation
+			if valueMap, ok := node.Value.(map[string]interface{}); ok {
+				log.Printf("Successfully found int value as map")
+				node.Value = &pb.IntValue{Value: int32(valueMap["value"].(int))}
+			}
+		case "FloatValue":
+			log.Printf("Found float value")
+			// Extract the actual float value from the representation
+			if valueMap, ok := node.Value.(map[string]interface{}); ok {
+				log.Printf("Successfully found float value as map")
+				node.Value = &pb.FloatValue{Value: float32(valueMap["value"].(float64))}
+			}
+		default:
+			log.Printf("Unknown type: %s", node.ValueType)
+			node.Value = nil // Handle unknown types
+		}
+	} else {
+		log.Printf("Loading values for node: %s", node.ValueType)
+	}
+	for _, child := range node.Children {
+		loadNodeValues(child)
+	}
+}
+
+// Helper function to parse the dataset value from the string representation
+func parseDatasetValue(str string) *pb.Dataset {
+	// Assuming the format is "map[Dataset:map[IndividualFile:map[column_names:[id name email] file_path:./tests/example_a.csv file_type:csv]]]"
+	parts := strings.Split(str, "map[IndividualFile:")
+	if len(parts) < 2 {
+		return nil
+	}
+	//individualFilePart := strings.Trim(parts[1], "]")
+
+	// Here you would parse individualFilePart to extract the actual values
+	// For demonstration, we will just return a new Dataset with dummy values
+	return &pb.Dataset{
+		Dataset: &pb.Dataset_IndividualFile{
+			IndividualFile: &pb.IndividualFile{
+				ColumnNames: []string{"id", "name", "email"},
+				FilePath:    "./tests/example_a.csv",
+				FileType:    "csv",
+			},
+		},
+	}
+}
