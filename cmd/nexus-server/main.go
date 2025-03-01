2e2753e5012a172dbbbc077db5bdd61a7500cf95
Elias Little <eliasjlittle@gmail.com>
2025-03-01 00:49:32 -0500

    Allows server to start new index from scratch

--- cmd/nexus-server/main.go
+++ cmd/nexus-server/main.go
@@ -14,11 +14,12 @@ import (
 )
 
 func printHelp() {
-	fmt.Println("Usage: 'nexus-server <load_file_path> <save_file_path>' or 'nexus-server <save_file_path>'")
+	fmt.Println("Usage: 'nexus-server <OPTIONS> <load_file_path> <save_file_path>' or 'nexus-server <save_file_path>'")
 	fmt.Println("Options:")
-	fmt.Println("  <load_file_path>   Path to the file to load.")
-	fmt.Println("  <save_file_path>   Path to the file to save.")
 	fmt.Println("  --help             Show this help message.")
+	fmt.Println("  --new-index        Create a new index.")
+	fmt.Println("<load_file_path>   Path to the file to load.")
+	fmt.Println("<save_file_path>   Path to the file to save.")
 }
 
 func main() {
@@ -27,9 +28,17 @@ func main() {
 
 	log.Info("Starting Nexus Server...")
 
+	new_index := false
 	// Check for help command
-	if len(os.Args) > 1 && os.Args[1] == "--help" {
-		printHelp()
+	if len(os.Args) > 1 {
+		if os.Args[1] == "--help" {
+			printHelp()
+		} else if os.Args[1] == "--new-index" {
+			new_index = true
+			if len(os.Args) != 3 {
+				log.Fatal("Usage: '%s --new-index <save_file_path>'", os.Args[0])
+			}
+		}
 	}
 
 	var fullLoadPath, fullSavePath string
@@ -40,7 +49,9 @@ func main() {
 	}
 
 	if len(os.Args) == 2 {
-		fullLoadPath = os.Args[1]
+		if !new_index {
+			fullLoadPath = os.Args[1]
+		}
 		fullSavePath = os.Args[1]
 	} else if len(os.Args) > 2 {
 		fullLoadPath = os.Args[1]
@@ -50,17 +61,24 @@ func main() {
 		log.Fatal("Usage: '%s <load_file_path> <save_file_path>' or '%s <save_file_path>'", os.Args[0], os.Args[0])
 	}
 
-	expandedPath, err := os.UserHomeDir() // Get the user's home directory
-	if err != nil {
-		log.Fatalf("Failed to get home directory: %v", err)
-	}
-	fullLoadPath = fmt.Sprintf("%s/%s", expandedPath, fullLoadPath) // Expand the load path
+	expandedPath := "/usr/local/etc/nexus" // Get the user's config directory
 	fullSavePath = fmt.Sprintf("%s/%s", expandedPath, fullSavePath)
 
-	log.Info("Creating new server", "path", fullLoadPath)
-	server, err := ns.NewServer(fullLoadPath)
-	if err != nil {
-		log.Fatal("Failed to create new server", "error", err)
+	var server *ns.NexusServer
+	var err error
+	if !new_index {
+		fullLoadPath = fmt.Sprintf("%s/%s", expandedPath, fullLoadPath) // Expand the load path
+		log.Info("Creating new server", "path", fullLoadPath)
+		server, err = ns.NewServer(fullLoadPath)
+		if err != nil {
+			log.Fatal("Failed to create new server", "error", err)
+		}
+	} else {
+		log.Info("Creating new server", "path", fullSavePath)
+		server, err = ns.NewServer("")
+		if err != nil {
+			log.Fatal("Failed to create new server", "error", err)
+		}
 	}
 
 	// Channel to listen for interrupt signals
